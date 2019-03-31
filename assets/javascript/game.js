da327cc77788d528eecbd2673ba1240bee96c90d
Kevin M <kevin.mahone19@gmail.com>
2019-03-31 10:12:51 -0400

    minor updates to code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,48 +7,57 @@ var $crystalOne = $("#crystalOne");
 var $crystalTwo = $("#crystalTwo");
 var $crystalThree = $("#crystalThree");
 var $crystalFour = $("#crystalFour");
-// create function that selects a random number between 19 - 120
-goal = Math.floor(Math.random() * 102) + 12;
 
-$("#randomGoal").text(goal);
+// documen.ready does not work
 
-// create a function that asigns a each crystal a number between 1 - 12
-// create a click event that adds crystal value to the userScore and displays it
-$crystalOne = Math.floor(Math.random() * 12) + 1;
-$crystalTwo = Math.floor(Math.random() * 12) + 1;
-$crystalThree = Math.floor(Math.random() * 12) + 1;
-$crystalFour = Math.floor(Math.random() * 12) + 1;
-console.log($crystalOne)
+    // create function that selects a random number between 19 - 120
+    goal = Math.floor(Math.random() * 102) + 12;
 
-$("#crystalOne").on("click", function () {
-    userScore += $crystalOne;
-})
-$("#crystalTwo").on("click", function () {
-    userScore = userScore + $crystalTwo
-}) 
-$("#crystalOne").on("click", function () {
-    userScore = userScore + $crystalTwo
-}) 
-$("#crystalOne").on("click", function () {
-    userScore = userScore + $crystalTwo
-})
+    $("#randomGoal").text(goal);
 
-$("#userScore").text(userScore);
+    // create a function that asigns a each crystal a number between 1 - 12
+    // create a click event that adds crystal value to the userScore and displays it
+    $crystalOne.attr("data-crystalvalue", Math.floor(Math.random() * 12) + 1);
+    $crystalTwo.attr("data-crystalvalue", Math.floor(Math.random() * 12) + 1);
+    $crystalThree.attr("data-crystalvalue", Math.floor(Math.random() * 12) + 1);
+    $crystalFour.attr("data-crystalvalue", Math.floor(Math.random() * 12) + 1);
 
-// create a function to start a new game
-function newGame() {
-    userScore = 0;
-    goal;
-};
+    $("#crystalOne").on("click", function () {
+        var crystalValue1 = ($(this).attr("data-crystalvalue"));
+        crystalValue1 = parseInt(crystalValue1);
+        userScore += $crystalOne;
+    });
+    $("#crystalTwo").on("click", function () {
+        var crystalValue2 = ($(this).attr("data-crystalvalue"));
+        crystalValue2 = parseInt(crystalValue2);
+        userScore += $crystalTwo;
+    });
+    $("#crystalThree").on("click", function () {
+        var crystalValue3 = ($(this).attr("data-crystalvalue"));
+        crystalValue3 = parseInt(crystalValue3);
+        userScore += $crystalThree;
+    });
+    $("#crystalFour").on("click", function () {
+        var crystalValue4 = ($(this).attr("data-crystalvalue"));
+        crystalValue4 = parseInt(crystalValue4);
+        userScore += $crystalFour;
+    });
+    // does not update
+    $("#userScore").text(userScore);
+    
+    // create a function to start a new game
+    function newGame() {
+        goal;
+    };
 
-// create a if else statement to track wins and losses and reset the game
-if (userScore === goal) {
-    win++;
-    newGame();
-} else if (userScore >= goal) {
-    lose++;
-    newGame();
-}
+    // create a if else statement to track wins and losses and reset the game
+    if (userScore === goal) {
+        win++;
+        newGame();
+    } else if (userScore >= goal) {
+        lose++;
+        newGame();
+    }
 
-$("#wins").text(win);
-$("#losses").text(losses);
+    $("#wins").text(win);
+    $("#losses").text(losses);
